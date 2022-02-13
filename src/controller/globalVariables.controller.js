e41251291f984e28f59b2daa8eabcc6336632896
PiyushDubey31 <piyushnit77@gmail.com>
2022-02-13 17:50:14 +0530

    added uppercase in global variable name

--- src/controller/globalVariables.controller.js
+++ src/controller/globalVariables.controller.js
@@ -10,7 +10,7 @@ module.exports = {
     try {
       const { variableName, value, description } = req.body;
       const response = new globalVariables({
-        variableName: variableName.toUpperCase().remove(" ", ""),
+        variableName: variableName.toUpperCase().replace(" ", ""),
         value,
         description,
       });
