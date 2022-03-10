867b32a5be4963fcdc49de4d67df31165f1e12f0
PiyushDubey31 <piyushnit77@gmail.com>
2022-03-10 17:02:11 +0530

    worked on settings

--- src/controller/settings.controller.js
+++ src/controller/settings.controller.js
@@ -52,7 +52,7 @@ module.exports = {
   getSettings: async (req, res) => {
     try {
       const { city } = req.body;
-      const response = await settings.findOne({ city: city.toLowerCase() });
+      const response = await settings.find({});
       OkResponse(res, response, "data fetched successfully");
     } catch (error) {
       ServerErrorResponse(res, error, "something went wrong");
